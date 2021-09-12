4da1e8de071507f9bf048d42aaf0a28c56fde5c8
gbjbuobbl <79749963+gbjbuobbl@users.noreply.github.com>
2021-09-12 15:44:21 +0530

    Add files via upload

---
+++ Intro-To-p5/main.js
@@ -0,0 +1,23 @@
+function preload() {
+
+}
+ function setup(){
+     canvas = creatCanvas(640 , 480);
+     canvas.position(110, 250);
+     video = creteCapture(VIDEO);
+     video.hide();
+     tint-color =  "";
+ }
+
+ function draw() {
+     Image(video, 0, 0, 640, 480);
+     tint(tint-color);
+ }
+
+ function take-snapshot() {
+     save("student-name.png");
+ }HUNDAYO
+
+ function Filter() {
+     tint-color = document.getElementById("color").value;
+ }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
